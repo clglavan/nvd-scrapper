469cedff3bf0c72a7c01e6e1a3a2cf343cec9ad9
Cristian Glavan <cristian.glavan@zitec.com>
2020-08-30 15:20:44 +0300

    refactor json format

--- .docker/feed.php
+++ .docker/feed.php
@@ -6,6 +6,8 @@ use Google\Cloud\Storage\StorageClient;
 try{
     $bucket = getenv('BUCKET');
     $projectId = getenv('PROJECTID');
+    $filename = getenv('FILENAME');
+    $format = getenv('FORMAT');
 
     $raw_data = file_get_contents('https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml');
 
@@ -13,32 +15,50 @@ try{
     unset($xml->channel);
 
     if($bucket){
-        $filename =  'results.json';
-        
         $fp = fopen($filename, 'w');
     }
+
+    $vulns = [];
 
     foreach ($xml as $item){
 
         /* get the CVE ID */ 
         $link_split = parse_url($item->link);
         parse_str($link_split['query'], $params); 
-        $vuln = [];
-        $vuln["vulnId"]     = $params['vulnId'];
 
-        $vuln["title"] = (string)$item->title;
-        $vuln["link"]  = (string)$item->link;
-        $vuln["description"] = (string)$item->description;
-        $vuln["@timestamp"] = (string)$item->xpath("dc:date")[0];
-        $vuln["source"] = "nvd.nist.gov";
-        
-        $vuln_json = json_encode($vuln);
-        print($vuln_json."\n");
+        if($format == 'json'){
+            $vuln = [];
+            $vuln["vulnId"]     = $params['vulnId'];
+            $vuln["title"] = (string)$item->title;
+            $vuln["link"]  = (string)$item->link;
+            $vuln["description"] = (string)$item->description;
+            $vuln["timestamp"] = (string)$item->xpath("dc:date")[0];
+            $vuln["source"] = "nvd.nist.gov";
+            
+            $vuln_json = json_encode($vuln);
+            print($vuln_json."\n");
 
-        if($bucket){
-            fwrite($fp, json_encode($vuln_json));
+            if($bucket){
+                $vulns[] = $vuln;
+            }
+
+        }else{
+            $controlchar="`";
+            $vuln = $params['vulnId'].$controlchar.(string)$item->title.$controlchar.(string)$item->link.$controlchar.(string)$item->description.$controlchar.(string)$item->xpath("dc:date")[0].$controlchar."nvd.nist.gov"."\r\n";
+            print($vuln);
+            
+            if($bucket){
+                // fputcsv($fp,)
+                fwrite($fp, $vuln);
+            }
         }
+        
+    }
 
+    if($format == 'json'){
+        if($bucket){
+            fwrite($fp, json_encode(["vulns" => $vulns]));
+        }
     }
 
     if($bucket){
